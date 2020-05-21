181edd5b1c7021f6eeb7e3aae20cad034b999dc3
Welly Shen <hivoid19@gmail.com>
2020-05-21 19:12:42 +0800

    upgrade deps and use plugin:testing-library/react extension

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:jest/recommended",
     "plugin:jest/style",
+    "plugin:testing-library/react",
     "plugin:prettier/recommended",
     "prettier/@typescript-eslint",
     "prettier/react",
