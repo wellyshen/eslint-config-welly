044e2bdc88f9f41beae57a0e9681d93e4e54a1ce
Welly Shen <hivoid19@gmail.com>
2021-05-20 22:12:03 +0800

    Fix: add `no-use-before-define` and `no-shadow` rules

--- index.js
+++ index.js
@@ -64,6 +64,8 @@ module.exports = {
     "jest/globals": true,
   },
   rules: {
+    "no-use-before-define": "off",
+    "no-shadow": "off",
     "react/jsx-props-no-spreading": "off",
     "react/jsx-filename-extension": [
       "error",
@@ -73,6 +75,8 @@ module.exports = {
     ],
     "import/extensions": "off",
     "import/no-extraneous-dependencies": "off",
+    "@typescript-eslint/no-use-before-define": "error",
+    "@typescript-eslint/no-shadow": "error",
     "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/no-explicit-any": "off",
     ...(hasEmotion
