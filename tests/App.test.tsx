8f457cee437427554efa2c801a91e9eb99e34481
Welly Shen <hivoid19@gmail.com>
2022-07-13 18:23:13 +0800

    test: refine test files

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -3,5 +3,8 @@ import { render, act } from "@testing-library/react";
 import App from ".App";
 
 it("should ...", () => {
+  const { debug } = render(<Hello />);
+  debug();
+
   expect(render(false)).toBeCalled(123);
 });
